8ef3b7689bb9f1c1c6fb18d70aadb9d1378cb5b9
scoutmimik <blazek.mikulas2@gmail.com>
2026-09-08 18:08:37 +0200

    Update ClearPauseMod.java

--- src/main/java/com/clearpause/ClearPauseMod.java
+++ src/main/java/com/clearpause/ClearPauseMod.java
@@ -24,7 +24,7 @@ import java.util.List;
 public class ClearPauseMod {
     public static final String MODID = "clearpause";
     public static final String NAME = "Clear Pause Menu";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0";
 
     private static final String[] BUTTON_LIST_FIELD = new String[]{"buttonList", "field_146292_n"};
 
